7fdb45d84d869b4394a0df84d5140614967ecc49
QHN21 <qhn21@wp.pl>
2020-05-10 14:24:48 +0200

    Prawie skonczono home page, dodano sign out button do zakladki profil

--- app/src/main/java/com/example/wpam/HomePage.kt
+++ app/src/main/java/com/example/wpam/HomePage.kt
@@ -1,19 +1,41 @@
 package com.example.wpam
 
 import android.os.Bundle
+import android.content.Intent
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
 
 class HomePage : AppCompatActivity() {
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var loginManager: LoginManager
+
+    private val RESULT_LOAD_IMAGE = 2
+    private lateinit var selectedImageBytes: ByteArray
+    private var pictureJustChanged = false
+    val PERMISSION_CODE = 1000
+    private val IMAGE_CAPTURE_CODE = 42
+
+    val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        loginManager = LoginManager.getInstance()
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
